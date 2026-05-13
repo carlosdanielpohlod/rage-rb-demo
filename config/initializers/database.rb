92018413a73c4ed0d1846ec7933420efbd46e175
carlosdanielpohlod <carlospohlod@gmail.com>
2026-05-13 20:03:40 -0300

    refactor: move schema to db/migrate

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,13 +1,4 @@
 Rage.config.after_initialize do
-  unless ActiveRecord::Base.connection.table_exists?(:posts)
-    ActiveRecord::Schema.define do
-      create_table :posts do |t|
-        t.string   :title, null: false
-        t.text     :body
-        t.datetime :created_at
-      end
-    end
-  end
-
+  ActiveRecord::MigrationContext.new("db/migrate").migrate
   load Rage.root.join("db/seeds.rb")
 end
---
+++ db/migrate/20260513000000_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :posts do |t|
+      t.string   :title, null: false
+      t.text     :body
+      t.datetime :created_at
+    end
+  end
+end
